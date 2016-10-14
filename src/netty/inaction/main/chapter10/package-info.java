7b4c714982adea1592cb18e9915f71d8a1585282
cmingjian <cmingjian@qq.com>
2016-10-15 05:17:22 +0800

    netty 单元测试的使用

---
+++ src/netty/inaction/main/chapter10/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * Contains code examples for chapter 9 of <i>Netty in Action</i>
+ *
+ * 本章的测试代码在test/netty.inaction.chapter10;
+ *
+ * 测试处理入站消息的handler
+ * Listing 10.1:    FixedLengthFrameDecoder.java
+ *
+ * 测试处理出站消息的handler
+ * Listing 10.2:    AbsIntegerEncoder.java
+ *
+ * Testing exception handling
+ * Listing 10.3:    FrameChunkDecoder.java
+ *
+ * @author <a href="mailto:norman.maurer@googlemail.com">Norman Maurer</a>
+ */
+package netty.inaction.main.chapter10;
